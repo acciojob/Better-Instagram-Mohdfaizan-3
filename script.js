c405f32640c91990ca4929ce9961ac0ebffa2a79
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-02-22 23:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+const divs = document.querySelectorAll(".image");
+let isDragging = false;
+let currentDrag;
+
+divs.forEach((div) => {
+  div.addEventListener("dragstart", function () {
+    isDragging = true;
+    currentDrag = this;
+  });
+
+  div.addEventListener("dragend", function () {
+    isDragging = false;
+  });
+
+  div.addEventListener("dragover", function (e) {
+    e.preventDefault();
+  });
+
+  div.addEventListener("dragenter", function (e) {
+    if (isDragging) {
+      this.classList.add("over");
+    }
+  });
+
+  div.addEventListener("dragleave", function () {
+    this.classList.remove("over");
+  });
+
+  div.addEventListener("drop", function () {
+    if (isDragging) {
+      let temp = currentDrag.innerHTML;
+      currentDrag.innerHTML = this.innerHTML;
+      this.innerHTML = temp;
+      this.classList.remove("over");
+    }
+  });
+});
